5677dd676114d5a59b9a6b9c9f8791c8fad449ca
Priminima <elinblomgren@gmail.com>
2015-03-01 16:21:43 +0100

    done 'Building Your First App'

--- MyFirstApplication/app/src/main/java/com/example/elin/myfirstapplication/DisplayMessageActivity.java
+++ MyFirstApplication/app/src/main/java/com/example/elin/myfirstapplication/DisplayMessageActivity.java
@@ -1,9 +1,10 @@
 package com.example.elin.myfirstapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -11,7 +12,12 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView tv = new TextView(this);
+        tv.setTextSize(40);
+        tv.setText(message);
+        setContentView(tv);
     }
 
 
