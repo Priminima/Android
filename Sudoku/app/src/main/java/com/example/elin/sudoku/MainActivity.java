40d77cb646e0d22208ad20394e517fe149885649
Priminima <elinblomgren@gmail.com>
2015-04-16 09:47:35 +0200

    fifteen game works but dosent look so good

--- Sudoku/app/src/main/java/com/example/elin/sudoku/MainActivity.java
+++ Sudoku/app/src/main/java/com/example/elin/sudoku/MainActivity.java
@@ -1,29 +1,49 @@
 package com.example.elin.sudoku;
 
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.GridLayout;
 
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
-public class MainActivity extends ActionBarActivity {
-
-    View gameMap[][] = new View[4][4];
-
+    Tile gameMap[][] = new Tile[4][4];
+    int row; //rowen som den tomma rutan är på
+    int col; //columnen som den tomma rutan är på
+    int numMoves;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GridLayout gMapL = (GridLayout) findViewById(R.id.gameMap);
+
+
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                String tvIJ = "tv" + i + "-" + j;
-                int resID = getResources().getIdentifier(tvIJ, "id", "com.example.elin.sudoku");
-                gameMap[i][j] = findViewById(resID);
+
+                gameMap[i][j] = new Tile(i, j, this);
+                gameMap[i][j].setText((16-(i*4+j)) + "");
+                gameMap[i][j].setHeight(80);
+                gameMap[i][j].setWidth(80);
+                gameMap[i][j].setOnClickListener(this);
+                gMapL.addView(gameMap[i][j]);
+
             }
         }
+
+        gameMap[0][0].setText("");
+        gameMap[0][0].setEnabled(false);
+        row = 0;
+        col = 0;
+
+
     }
 
 
@@ -49,8 +69,94 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onMoveTile(View v) {
+    public void onClick(View v) {
 
+        Tile t = (Tile) findViewById(v.getId());
+        int i = t.r;
+        int j = t.c;
 
+        if (row == i) {
+            if (col > j) {
+                while (col - 1 >= j){
+                    oneMove(i, col - 1);
+                }
+            } else {
+                while (col + 1 <= j){
+                    oneMove(i, col + 1);
+                }
+            }
+        } else if (col == j) {
+            if (row > i) {
+                while (row - 1 >= i){
+                    oneMove(row - 1, j);
+                }
+            } else {
+                while (row + 1 <= i){
+                    oneMove(row + 1, j);
+                }
+            }
+        } else {
+
+        }
+    }
+
+    public void oneMove(int i, int j) {
+        gameMap[row][col].setText(gameMap[i][j].getText());
+        gameMap[row][col].setEnabled(true);
+        gameMap[i][j].setText("");
+        gameMap[i][j].setEnabled(false);
+        row = i;
+        col = j;
+        numMoves++;
+        //d.setText("Antal drag: " + drag);
     }
 }
+
+class Tile extends Button {
+    int r;
+    int c;
+    public Tile (int i, int j, Context context) {
+        super(context);
+        super.setId(1 + j + i*4);
+        r = i;
+        c = j;
+    }
+}
+
+/*if (row == i) {
+if (col > j) {
+        while (col - 1 >= j){
+        enFlytt(i, col - 1);
+        }
+        } else {
+        while (col + 1 <= j){
+        enFlytt(i, col + 1);
+        }
+        }
+        } else if (col == j) {
+        if (row > i) {
+        while (row - 1 >= i){
+        enFlytt(row - 1, j);
+        }
+        } else {
+        while (row + 1 <= i){
+        enFlytt(row + 1, j);
+        }
+        }
+        } else {
+        Toolkit.getDefaultToolkit().beep();
+        }
+
+
+
+        public void enFlytt(int i, int j) {
+		ruta[row][col].setText(ruta[i][j].getText());
+		ruta[row][col].setEnabled(true);
+		ruta[i][j].setText("");
+		ruta[i][j].setEnabled(false);
+		row = i;			col = j;
+		drag++;
+		d.setText("Antal drag: " + drag);
+	}
+
+*/
--- Sudoku/app/src/main/java/com/example/elin/sudoku/fifteenLogic.java
+++
@@ -1,9 +0,0 @@
-package com.example.elin.sudoku;
-
-/**
- * Created by elin on 2015-04-13.
- */
-public class fifteenLogic {
-
-
-}
